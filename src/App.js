d9aca3b8ecbac4b777b200d4ed4a6044ea4203ec
johnyip <johngyip@gmail.com>
2025-04-09 16:51:22 -0700

    message 02

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Pack my box with five dozen liquor jugs
+         
         </p>
+        <p>The quick brown fox jumps over the lazy dog</p>
         <a
           className="App-link"
           href="https://reactjs.org"
